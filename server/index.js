943181240dd60464ebd3fffc0163516a0ade23ec
sarah-tech21 <sarah-tech21@users.noreply.github.com>
2026-05-02 05:14:07 +0000

    deploy: e030ec8c1cad0ce61745cbd2707843b3106e06f1

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import { _, $ } from "./assets/worker-entry-C_Xjei1k.js";
-import "node:events";
-import "node:async_hooks";
-import "node:stream/web";
-import "node:stream";
-export {
-  _ as createServerEntry,
-  $ as default
-};
